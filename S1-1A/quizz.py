325c1009712f38acf75b182428c9ce4142fc8d97
Remi17 <remi.tang@hotmail.fr>
2019-02-13 14:16:54 +0100

    ajout de la question quizz

--- S1-1A/quizz.py
+++ S1-1A/quizz.py
@@ -1,3 +1,7 @@
 # -*- coding: utf-8 -*-
 
-quizz = input("bonjour")
+reponse = input("Voici ma question")
+if reponse == "vraie":
+    print("Bravo, c'est la bonne réponse")
+else:
+    print("Dommage, ce n'est pas la bonne réponse")
